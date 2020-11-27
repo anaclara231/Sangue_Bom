281ea37b04aad30126a6b55b6f863fddd0bff6e9
Carlos Eduardo <kaduce2009@hotmail.com>
2020-11-27 12:47:42 -0300

    deitando no css

--- JS/validarSenha.js
+++ JS/validarSenha.js
@@ -6,10 +6,17 @@ function validarLogin(){
     let senha2 = document.getElementById('SENHA_USUARIO2').value;
     
     if (senha1 != senha2) {
+        document.getElementById('SENHA_USUARIO').style.borderColor='red';
+        document.getElementById('SENHA_USUARIO2').style.borderColor='red';
+        document.getElementById('senhaErro').style.display='block';
         return false
       
+    }else{
+        document.getElementById('senhaErro').style.display="none";
+        return true;
+
     }
-    return true;
+    
         
           
     
