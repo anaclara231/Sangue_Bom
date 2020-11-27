a9a45018a4ecb832141ebb13802ba680e2e088c1
Carlos Eduardo <kaduce2009@hotmail.com>
2020-11-27 10:08:31 -0300

    validaçãodaSenha

---
+++ JS/validarSenha.js
@@ -0,0 +1,16 @@
+
+
+
+function validarLogin(){
+    let senha1 = document.getElementById('SENHA_USUARIO').value;
+    let senha2 = document.getElementById('SENHA_USUARIO2').value;
+    
+    if (senha1 != senha2) {
+        return false
+      
+    }
+    return true;
+        
+          
+    
+ }
