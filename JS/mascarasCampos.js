c224dd4b3d231499afd383c95e6dde4ad1646891
unknown <thiagojolvino@gmail.com>
2020-11-29 14:21:27 -0300

    inputs masks

---
+++ JS/mascarasCampos.js
@@ -0,0 +1,37 @@
+    function mascara_cpf(){
+    let cpf = document.getElementById('CPF')
+    if(cpf.value.length == 3 || cpf.value.length == 7 ) {
+    cpf.value += "."
+    } else if(cpf.value.length == 11) {
+        cpf.value += "-"
+    }
+ }
+    function mascara_rg(){
+        let rg = document.getElementById('RG')
+        if(rg.value.length == 2 || rg.value.length == 6 ) {
+            rg.value += "."
+        }else if 
+            (rg.value.length == 10) {
+                rg.value += "-"
+            }
+    }
+    function mascara_cep(){
+        let cep = document.getElementById('CEP')
+        if (cep.value.length == 5) {
+            cep.value += "-"
+        }
+    }
+$(document).ready(function(){
+    var SPMaskBehavior = function (val) {
+        return val.replace(/\D/g, '').length === 11 ? '(00) 00000-0000' : '(00) 0000-00009';
+      },
+      spOptions = {
+        onKeyPress: function(val, e, field, options) {
+            field.mask(SPMaskBehavior.apply({}, arguments), options);
+          }
+      };
+      
+      $('#NUM_TELEFONE').mask(SPMaskBehavior, spOptions);
+})
+    
+    
--- JS/validarSenha.js
+++ JS/validarSenha.js
@@ -1,6 +1,4 @@
 
-
-
 function validarLogin(){
     let senha1 = document.getElementById('SENHA_USUARIO').value;
     let senha2 = document.getElementById('SENHA_USUARIO2').value;
@@ -21,3 +19,4 @@ function validarLogin(){
           
     
  }
+
