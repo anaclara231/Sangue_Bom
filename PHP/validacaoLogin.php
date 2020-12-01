f3c44b2fba70c9df978c4ad6651a33de78def673
unknown <thiagojolvino@gmail.com>
2020-11-30 22:06:04 -0300

    validacao login

--- PHP/validacaoLogin.php
+++ PHP/validacaoLogin.php
@@ -1,21 +1,25 @@
 <?php
 include_once('conexao.php');
 
-$emailLogin = $_POST('EMAIL_USUARIO');
-$senhaLogin = $_POST('SENHA_USUARIO');
+$emailLogin = $_POST['EMAIL_USUARIO'];
+$senhaLogin = $_POST['SENHA_USUARIO'];
 
-$validacaoLogin =  "SELECT * FROM tbl_login_usuario WHERE EMAIL_LOGIN = '$emailLogin' and SENHA_USUARIO = '$senhaLogin'";
+$validacaoLogin =  "SELECT * FROM tbl_login_usuario WHERE email_usuario = '$emailLogin' and senha_usuario = '$senhaLogin'";
 
 $resultadovalidacao=mysqli_query($strcon, $validacaoLogin);
 $resultado = mysqli_fetch_array($resultadovalidacao);
 
+mysqli_close($strcon);
+
 if ($resultado){
     echo "<script>
-        window.location.href='dashboard.html';
-    </script>"
+        window.location.href='../dashboard.html';
+    </script>";
 }else{
-    
-echo "Nenhum usuario foi encontrado";
+echo"
+<script>
+window.location.href='../login.html'
+</script>";
 }
 
 ?>
